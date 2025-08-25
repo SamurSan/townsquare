d781981a8145dd0608d7d1ce66a0306ca4af8dc0
unknown <sephyroth142@gmail.com>
2025-08-25 23:34:26 +0200

    googlesex2

--- src/store/socket.js
+++ src/store/socket.js
@@ -1,7 +1,7 @@
 class LiveSession {
   constructor(store) {
-    //this._wss = "https://townsquare-2pt2.vercel.app/";
-    this._wss = "ws://localhost:8081/"; // uncomment if using local server with NODE_ENV=development
+    this._wss = "https://townsquare-2pt2.vercel.app/";
+    //this._wss = "ws://localhost:8081/"; // uncomment if using local server with NODE_ENV=development
     this._socket = null;
     this._isSpectator = true;
     this._gamestate = [];
