f1f397b5ae71249692a60f885f31cca0cb29db42
unknown <sephyroth142@gmail.com>
2025-08-27 20:23:28 +0200

    sanka1

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const wss = new WebSocket.Server({
   verifyClient: info =>
     info.origin &&
     !!info.origin.match(
-      /^https?:\/\/([^.]+\.github\.io|localhost|clocktower\.online|eddbra1nprivatetownsquare\.xyz)/i
+      /^https?:\/\/([^.]+\.github\.io|localhost|clocktower\.online|townsquare-2pt2\.vercel\.app|eddbra1nprivatetownsquare\.xyz)/i
     )
 });
 
